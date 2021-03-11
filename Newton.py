67e86871408b7c17fffc580b6ea7a2bb6adbe049
Henry Song <henry.song671@gmail.com>
2021-03-11 23:01:03 +1000

    commented and fixed newton's method

--- Newton.py
+++ Newton.py
@@ -2,24 +2,33 @@
 # Homework #4: Root-Finding Methods
 # File: Newton.py
 # Description: Method for Newton's method. Default iteration is set to 1000.
-#   - Inputs:(f, interval)
+#   - Inputs:(f, interval, x0)
 #   --- f: lambda representation of function
 #   --- interval: array contanining search interval (i.e. [-2, 0])
+#   --- x0: initial guess for root location
 #   - Output: approximation of root or None if DNE
 #==========================================================================
 
-def newton(f, f_prime, x0, epsilon):
-    max_iter = 1000
-    xn = x0
-    for n in range(0,max_iter):
-        fxn = f(xn)
-        if abs(fxn) < epsilon:
-            print('Found solution after',n,'iterations.')
-            return xn
-        f_primexn = f_prime(xn)
-        if f_primexn == 0:
-            print('Zero derivative. No solution found.')
-            return None
-        xn = xn - fxn/f_primexn
-    print('Exceeded maximum iterations. No solution found.')
-    return None
+def newton(f, f_prime, x0):
+    # default iteration is set to 1000
+    n = 1000
+
+    # default error tolerance is set to 0.000001
+    E = 0.000001
+
+    r = x0 - f(x0)/f_prime(x0)
+
+    for i in range(n):
+        # checks if root falls into error tolerance
+        if abs(r) >= E: 
+            # checks if |f(x0)| < E
+            if abs(f(x0)) < E: return x0
+
+            xn = x0
+            x0 = x0 - f(x0)/f_prime(x0)
+
+            # checks if |xn-x| < E
+            if abs(xn - x0) < E: return x0
+        else:
+            break
+    return r
